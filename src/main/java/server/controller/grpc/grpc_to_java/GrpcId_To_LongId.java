3241f13852669214dcf5690580bbc9b6d5632d58
Dogmeat2000 <86206683+Dogmeat2000@users.noreply.github.com>
2024-10-14 14:14:01 +0200

    Finished implementing the PartType registration and management functionality.

--- src/main/java/server/controller/grpc/grpc_to_java/GrpcId_To_LongId.java
+++ src/main/java/server/controller/grpc/grpc_to_java/GrpcId_To_LongId.java
@@ -1,6 +1,8 @@
 package server.controller.grpc.grpc_to_java;
 
 import grpc.AnimalId;
+import grpc.AnimalPartId;
+import grpc.PartTypeId;
 import grpc.TrayId;
 
 /** <p>Responsible for converting gRPC compatible id's into application/java compatible id formats</p> */
@@ -10,6 +12,19 @@ public class GrpcId_To_LongId
   public static long ConvertToLongId(AnimalId animal_id) {
     return animal_id.getAnimalId();
   }
+
+
+  /** <p>Convert a gRPC data type's id into a java compatible id format</p> */
+  public static long ConvertToLongId(AnimalPartId animalPartid) {
+    return animalPartid.getAnimalPartId();
+  }
+
+
+  /** <p>Convert a gRPC data type's id into a java compatible id format</p> */
+  public static long ConvertToLongId(PartTypeId partTypeid) {
+    return partTypeid.getPartTypeId();
+  }
+
 
   /** <p>Convert a gRPC data type's id into a java compatible id format</p> */
   public static long ConvertToLongId(TrayId trayId) {
--- src/main/java/server/grpc/GrpcAnimalPartServiceImpl.java
+++ src/main/java/server/grpc/GrpcAnimalPartServiceImpl.java
@@ -9,10 +9,14 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import server.controller.grpc.grpc_to_java.GrpcAnimalPartData_To_AnimalPart;
+import server.controller.grpc.grpc_to_java.GrpcId_To_LongId;
 import server.controller.grpc.java_to_gRPC.AnimalPart_ToGrpc_AnimalPartData;
+import server.controller.grpc.java_to_gRPC.Animal_ToGrpc_AnimalData;
 import server.service.AnimalPartRegistryInterface;
+import shared.model.entities.Animal;
 import shared.model.entities.AnimalPart;
 import shared.model.exceptions.CreateFailedException;
+import shared.model.exceptions.NotFoundException;
 
 @GrpcService
 public class GrpcAnimalPartServiceImpl extends SlaughterHouseServiceGrpc.SlaughterHouseServiceImplBase
@@ -33,7 +37,7 @@ public class GrpcAnimalPartServiceImpl extends SlaughterHouseServiceGrpc.Slaught
       // attempt to register the AnimalPart:
       AnimalPart createdAnimalPart = animalPartService.registerAnimalPart(GrpcAnimalPartData_To_AnimalPart.convertToAnimalPart(request));
 
-      // If animal creation fails
+      // If animalPart creation fails
       if (createdAnimalPart == null)
         throw new CreateFailedException("AnimalPart could not be created");
 
@@ -48,7 +52,24 @@ public class GrpcAnimalPartServiceImpl extends SlaughterHouseServiceGrpc.Slaught
 
   @Override
   public void readAnimalPart(AnimalPartId request, StreamObserver<grpc.AnimalPartData> responseObserver) {
-    //TODO MISSING IMPLEMENTATION
+    // TODO: Missing implementation!
+    /*try {
+      // Translate received gRPC information from the client, into Java compatible type,
+      // and attempt to read the AnimalPart with the provided ID:
+      AnimalPart animalPart = animalPartService.readAnimalPart(GrpcId_To_LongId.ConvertToLongId(request), request.get);
+
+      // If Animal read failed:
+      if (animalPart == null)
+        throw new NotFoundException("Animal not found");
+
+      // Translate the found Animal into gRPC compatible types, before transmitting back to client:
+      responseObserver.onNext(Animal_ToGrpc_AnimalData.convertToAnimalData(animalPart));
+      responseObserver.onCompleted();
+    } catch (NotFoundException e) {
+      responseObserver.onError(Status.NOT_FOUND.withDescription("Animal with id " + request.getAnimalId() + "not found in DB").withCause(e).asRuntimeException());
+    } catch (Exception e) {
+      responseObserver.onError(Status.INTERNAL.withDescription("Error reading animal").withCause(e).asRuntimeException());
+    }*/
   }
 
 
